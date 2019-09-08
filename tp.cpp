8f36a7709138f946c09773f965b9cc48bc136476
Jeet <jeetjpavani@gmail.com>
2019-09-08 23:15:45 +0530

    inversion

---
+++ tp.cpp
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main(){
+	int i = 0;
+	int arr[6];
+	arr[i++] = i;
+	cout<<arr[0];
+	cout<<endl;
+	cout<<arr[1];
+}
